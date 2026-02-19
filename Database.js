80dd3b329c634e0468c6604b3f311c62fee10271
zyntax6760 <kinghojoon12@gmail.com>
2026-02-19 23:59:21 +0900

    디렉토리 변경 및 게임 추가
    - 디렉토리 수정
    - "홀짝", "복권" 추가
    - DB로 유저 관리

---
+++ Database.js
@@ -0,0 +1,14 @@
+const Database = require("better-sqlite3");
+const path = require("path");
+const db = new Database(path.join(__dirname, "game.db"));
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS user (
+        user_id TEXT PRIMARY KEY,
+        money INTEGER DEFAULT 1000
+    )
+`);
+
+console.log("Connect Database Success.");
+
+module.exports = db;
